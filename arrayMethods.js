9ef792fdbba537b596b73b7cc27721e211c3e52c
Mia Sanchez <mia@Claudias-MacBook-Air.local>
2021-07-01 14:59:00 -0400

    first commit message arrays

---
+++ arrayMethods.js
@@ -0,0 +1,85 @@
+// const fruits = [
+//     'Bannana',
+//     'Orange',
+//     'Apple'
+// ]
+
+// fruits.push('Lemons')
+// fruits.push({newFruit: '🍎'})
+// fruits.unshift({pet : '🐠'})
+
+
+// // console.log('Should get Orange', fruits[1])
+// // console.log('this length', fruits.length)
+// console.log('These are my fruits',fruits)
+
+// const shoppingList = ['shampoo', 'milk','soap', '🥓', '🍣']
+// // console.log('this is shopping list =============',shoppingList)
+
+// const newShoppingList =  shoppingList.concat(fruits)
+// // console.log('new shopping list here',newShoppingList.sort ())
+// console.log(newShoppingList.reverse())
+// console.log(newShoppingList.toString())
+// console.log(newShoppingList.toString())
+
+// const cars = [{ford: 'truck'},'audi','bmw','vw','nissan','mazda','honda','hyundai','bus']
+
+// console.log(cars.toString())
+// // console.log(cars.join('== * =='))
+// cars.push(['honda','toyota','mitsubishi'])
+// console.log(cars) 
+
+
+
+// the assignment for wednesday
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('fizzbuzz')
+
+//     } else if (i % 3 === 0) {
+//         console.log('fizz')
+
+//     } else if (i % 5 === 0) {
+//         console.log('buzz')
+
+//     } else {
+//         console.log(i)
+//     }
+
+// }
+
+
+// function isEven() {
+//     return
+// }
+
+
+// let row = []
+//  for (let i = 1; i <= 5 ; i++){
+//    row.push('#')
+//      console.log(row.join (''))   
+// }
+
+
+
+console.log('Start!')
+setTimeout ( () =>{
+    console.log ('Middle')
+
+}, 5000) 
+console.log ('Done')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
